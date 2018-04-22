baadec334b75ca316a95e868313df55cb3d62acd
zidaneandmessi <dimar@qq.com>
2018-04-22 11:07:52 +0800

    SDAFRE

--- src/gzotpa/compiler/Compiler.java
+++ src/gzotpa/compiler/Compiler.java
@@ -6,4 +6,48 @@ import gzotpa.ast.ExprNode;
 import java.util.*;
 import java.io.*;
 
-public class Compiler {}
+public class Compiler {
+    static final public String ProgramName = "laatiessaan";
+
+    static public void main(String[] args) {
+        new Compiler(ProgramName).commandMain(args);
+    }
+
+    public void commandMain(String[] args) {
+        Options opts = parseOptions(args);
+        if (opts.mode() == CompilerMode.CheckSyntax) {
+            System.exit(checkSyntax(opts) ? 0 : 1);
+        }
+    }
+    private Options parseOptions(String[] args) {
+        return Options.parse(args);
+    }
+
+    private boolean checkSyntax(Options opts) {
+        boolean failed = false;
+        for (SourceFile src : opts.sourceFiles()) {
+            if (isValidSyntax(src.path(), opts)) {
+                System.out.println(src.path() + ": Syntax OK");
+            }
+            else {
+                System.out.println(src.path() + ": Syntax Error");
+                failed = true;
+            }
+        }
+        return !failed;
+    }
+
+    private boolean isValidSyntax(String path, Options opts) {
+        try {
+            parseFile(path, opts);
+            return true;
+        }
+        catch (SyntaxException ex) {
+            return false;
+        }
+        catch (FileException ex) {
+            errorHandler.error(ex.getMessage());
+            return false;
+        }
+    }
+}
